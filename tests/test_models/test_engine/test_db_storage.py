6402c7900c38d80cd3d8675f1c08766da2da8772
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-05-01 13:38:07 +0300

    Add get and count methods to DBStorage and FileStorage

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -3,21 +3,23 @@
 Contains the TestDBStorageDocs and TestDBStorage classes
 """
 
-from datetime import datetime
 import inspect
 import models
 from models.engine import db_storage
 from models.amenity import Amenity
-from models.base_model import BaseModel
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-import json
-import os
 import pep8
 import unittest
+from sqlalchemy import text
+from sqlalchemy.orm import sessionmaker
+from models import storage
+from models.base_model import Base
+
+
 DBStorage = db_storage.DBStorage
 classes = {"Amenity": Amenity, "City": City, "Place": Place,
            "Review": Review, "State": State, "User": User}
@@ -86,3 +88,108 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+
+class TestDBStorageGetMethod(unittest.TestCase):
+    """Unittests for get method of db storage module"""
+
+    @classmethod
+    def setUpClass(cls):
+        """DBStorage testing setup."""
+        if type(models.storage) == DBStorage:
+            cls.storage = DBStorage()
+            Base.metadata.create_all(cls.storage._DBStorage__engine)
+            Session = sessionmaker(bind=cls.storage._DBStorage__engine)
+            cls.storage._DBStorage__session = Session()
+            cls.storage._DBStorage__session.commit()
+
+    @classmethod
+    def tearDownClass(cls):
+        """DBStorage testing teardown."""
+        if type(models.storage) == DBStorage:
+            # Remove all records from the tables
+            cls.storage._DBStorage__session.execute(
+                text("DELETE FROM amenities"))
+            cls.storage._DBStorage__session.execute(text("DELETE FROM cities"))
+            cls.storage._DBStorage__session.execute(text("DELETE FROM places"))
+            cls.storage._DBStorage__session.execute(
+                text("DELETE FROM reviews"))
+            cls.storage._DBStorage__session.execute(text("DELETE FROM states"))
+            cls.storage._DBStorage__session.execute(text("DELETE FROM users"))
+
+            cls.storage._DBStorage__session.commit()
+
+            # Delete the session
+            cls.storage._DBStorage__session.close()
+
+            # Delete the storage
+            del cls.storage
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get_with_amenity_wrong_id(self):
+        """test get with amenity with wrong id"""
+        self.assertEqual(storage.get(Amenity, '12345'), None)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get_with_city_wrong_id(self):
+        """test get with city with wrong id"""
+        self.assertEqual(storage.get(City, '12345'), None)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get_with_place_wrong_id(self):
+        """test get with place with wrong id"""
+        self.assertEqual(storage.get(Place, '12345'), None)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get_with_review_wrong_id(self):
+        """test get with review with wrong id"""
+        self.assertEqual(storage.get(Review, '12345'), None)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get_with_state_wrong_id(self):
+        """test get with state with wrong id"""
+        self.assertEqual(storage.get(State, '12345'), None)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get_with_user_wrong_id(self):
+        """test get with user with wrong id"""
+        self.assertEqual(storage.get(User, '12345'), None)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get_with_amenity_valid_id(self):
+        """test get with amenity with valid id"""
+        amenity = Amenity(name="Anything")
+        amenity.save()
+        amenity_from_get = storage.get(Amenity, amenity.id)
+        self.assertEqual(amenity_from_get.to_dict(), amenity.to_dict())
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get_with_city_valid_id(self):
+        """test get with city with valid id"""
+        state = State(name="Anything")
+        state.save()
+        city = City(name="city_name", state_id=state.id)
+        city.save()
+        city_from_get = storage.get(City, city.id)
+        self.assertEqual(city_from_get.to_dict(), city.to_dict())
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count_without_cls(self):
+        """Test count without cls"""
+        amenity = Amenity(name="amenity_name")
+        amenity.save()
+        state = State(name="state_name")
+        state.save()
+        city = City(name="city_name", state_id=state.id)
+        city.save()
+        user = User(fisrt_name="real",
+                    last_name="3bdelrahman",
+                    email="abdelrahman@gmail.com",
+                    password="winner-winner_chicken-dinner")
+        user.save()
+
+        self.assertEqual(storage.count(), 4)
+        self.assertEqual(storage.count(), len(storage.all()))
+
+
+if __name__ == "__main__":
+    unittest.main()

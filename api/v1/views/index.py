caf37dc836842df7613d41a58c6656178a2319e6
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-05-02 07:05:01 +0300

    Add route handler for /stats endpoint in index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -18,7 +18,14 @@ the application.
 """
 
 from api.v1.views import app_views
+from models import storage
 from flask import jsonify
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route("/status")
@@ -31,3 +38,30 @@ def status():
     json: A JSON response with the status of the application.
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def count():
+    """
+    This function is a route handler for the "/stats" endpoint.
+    It counts the number of instances for each object type in the storage.
+
+    The object types are: Amenity, City, Place, Review, State, and User.
+
+    Returns:
+        A JSON object where the keys are the names of the object types and
+        the values are the counts of instances for each type.
+    """
+    objN = {
+        "amenities": Amenity,
+        "cities": City,
+        "places": Place,
+        "reviews": Review,
+        "states": State,
+        "users": User
+        }
+
+    obj_counts = {
+        key: storage.count(value) for key, value in objN.items()
+        }
+    return jsonify(obj_counts)

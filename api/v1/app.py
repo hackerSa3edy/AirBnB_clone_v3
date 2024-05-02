581505a6744ecf1f6a32a0418010bd625a4d5ebe
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-05-02 19:06:30 +0300

    Fix error handler for 404 Not Found in app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -24,7 +24,7 @@ It also enables multithreading.
 
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -63,7 +63,7 @@ def err404(error):
         and a value "Not found",
         and the second element is the HTTP status code 404.
     """
-    return jsonify({"error": "Not found"}), 404
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == '__main__':

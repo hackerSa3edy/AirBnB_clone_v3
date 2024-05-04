f5414cfd775662aff3b71fe0508cc297ca3c4824
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-05-04 03:56:46 +0300

    Update password hashing logic in User class

--- models/user.py
+++ models/user.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """ holds class User"""
+from typing import Any
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -27,3 +29,8 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        if name == 'password':
+            value = hashlib.md5(bytes(str(value), 'utf-8')).hexdigest()
+        return super().__setattr__(name, value)
